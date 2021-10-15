234b92ca925a09971ce2296ae9a479e3a4596411
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-15 16:40:05 +0200

    es. 1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ console.log(`Il numero inserito è ${numero}`);
 
 //Controllo numero con resto
 if ((numero % 2) === 0) {
+    console.log(`Il numero ${numero} è pari`);
     alert(`Il numero ${numero} è pari`);
 } else {
+    console.log(`Il numero ${numero} è dispari`);
     alert(`Il numero ${numero} è dispari`);
 }
